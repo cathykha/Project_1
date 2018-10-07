97ea8952dd939fdfab305805a3c2a2e5470d1738
cathykh <pk552@njit.edu>
2018-10-06 23:33:20 -0400

    Feature: Reading csv into html table

--- public/index.php
+++ public/index.php
@@ -7,69 +7,122 @@
  */
 
 main::start("temp.csv");
-class main  {
-    static public function start($filename) {
+
+class main {
+
+    public static function start($filename){
+
         $records = csv::getRecords($filename);
         $table = html::generateTable($records);
     }
 }
-class html {
-    public static function generateTable($records) {
-        $count = 0;
+
+class html
+{
+
+
+    public static function generateTable($records)
+    {
+        $table = "";
+        $table .= "<table class = 'table table-striped'>";
+
         foreach ($records as $record) {
-            if($count == 0) {
-                $array = $record->returnArray();
-                $fields = array_keys($array);
-                $values = array_values($array);
-                print_r($fields);
-                print_r($values);
-            } else {
-                $array = $record->returnArray();
-                $values = array_values($array);
-                print_r($values);
-            }
-            $count++;
+
+            $array = $record->returnArray();
         }
+
+        $table .= "<tr>";
+
+        foreach ($record as $key => $values) {
+
+            $table .= "<tr>";
+            echo "<th>" . $key . "</th>";
+            $table .= "</tr>";
+
+        }
+
+        foreach ($records as $record) {
+
+            $array = $record->returnArray();
+            $table .= "<tr>";
+
+            foreach ($record as $key => $values) {
+                $table .= "<td>" . $values . "</td>";
+
+            }
+            $table .= "</tr>";
+        }
+        $table .= "</table>";
+        print_r($table);
     }
 }
 class csv {
-    static public function getRecords($filename) {
-        $cvsfile = fopen($filename,"r");
+
+
+
+    public static function getRecords($filename) {
+
+
+        $csvfile = fopen($filename,"r");
         $fieldNames = array();
         $count = 0;
-        while(! feof($cvsfile))
-        {
-            $record = fgetcsv($cvsfile);
+
+        while(! feof($csvfile)) {
+
+            $record = fgetcsv($csvfile);
+
             if($count == 0) {
                 $fieldNames = $record;
+
             } else {
                 $records[] = recordFactory::create($fieldNames, $record);
+
             }
             $count++;
+
         }
-        fclose($cvsfile);
+
+        fclose($csvfile);
+
         return $records;
     }
 }
 class record {
-    public function __construct(Array $fieldNames = null, $values = null )
-    {
+
+
+
+    public function __construct(Array $fieldNames, $values){
+
+
         $record = array_combine($fieldNames, $values);
         foreach ($record as $property => $value) {
             $this->createProperty($property, $value);
         }
     }
+
+
     public function returnArray() {
         $array = (array) $this;
         return $array;
     }
-    public function createProperty($name = 'first', $value = 'keith') {
+
+
+    public function createProperty($name = 'first', $value = 'cathy') {
         $this->{$name} = $value;
     }
 }
+
 class recordFactory {
-    public static function create(Array $fieldNames = null, Array $values = null) {
+
+
+
+    public static function create(Array $fieldNames, Array $values) {
+
+
         $record = new record($fieldNames, $values);
+
         return $record;
     }
 }
+
+
